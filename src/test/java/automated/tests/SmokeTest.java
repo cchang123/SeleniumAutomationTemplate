a2dad77982ccfe9ce112cb97d518a305b4c36fa4
Chang <Chiwei.Chang@partner.vermont.gov>
2022-06-27 11:41:02 -0400

    Initial commit

---
+++ src/test/java/automated/tests/SmokeTest.java
@@ -0,0 +1,31 @@
+package automated.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+	
+	String baseURL = "https://www.google.com";
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-features=VizDisplayCompositor");
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
+		driver = new ChromeDriver(options);
+		wait = new WebDriverWait(driver, 60000);
+	}
+	
+  @Test
+  public void f() {
+	  driver.get(baseURL);
+  }
+}
